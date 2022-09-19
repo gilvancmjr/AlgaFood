f76c000ac6a0e7604140827dd654f94e8270369e
gilvancarvalho <gilvancarvalho@sefaz.pi.gov.br>
2022-09-19 12:53:39 -0300

    Implementação do RestauranteRepositoryImpl

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -11,11 +11,11 @@ import org.springframework.stereotype.Repository;
 import com.algaworks.algafood.domain.model.Restaurante;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries {
 
 	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
-	//@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+	// @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
 	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
 
 //	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
@@ -25,4 +25,5 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 	List<Restaurante> findTop2ByNomeContaining(String nome);
 
 	int countByCozinhaId(Long cozinha);
+
 }
