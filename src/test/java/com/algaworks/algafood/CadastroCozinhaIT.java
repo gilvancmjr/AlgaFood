27f0728b5c9dc88444b3964327ef9fc37293816c
gilvancmjr <juniormonteiroti@gmail.com>
2022-12-02 17:56:43 -0300

    Error no teste deveRetornarStatus201_QuandoCadastrarCozinha

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,45 +1,98 @@
 package com.algaworks.algafood;
 
-import org.aspectj.lang.annotation.Before;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.util.DatabaseCleaner;
+import com.algaworks.algafood.util.ResourceUtils;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class CadastroCozinhaIT {
+@TestPropertySource("/application-test.properties")
+public class CadastroCozinhaIT {
 
-	// TESTE DE API
+	private static final int COZINHA_ID_INEXISTENTE = 100;
 
 	@LocalServerPort
 	private int port;
 
+	@Autowired
+	private DatabaseCleaner databaseCleaner;
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	private Cozinha cozinhaAmericana;
+	private int quantidadeCozinhasCadastradas;
+	private String jsonCorretoCozinhaChinesa;
+
 	@BeforeEach
 	public void setUp() {
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();// mostra o retorno do teste
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
+
+		jsonCorretoCozinhaChinesa = ResourceUtils.getContentFromResource("/json/correto/cozinha-chinesa.json");
+
+		databaseCleaner.clearTables();
+		prepararDados();
 	}
 
 	@Test
 	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
-
 		RestAssured.given().accept(ContentType.JSON).when().get().then().statusCode(HttpStatus.OK.value());
 	}
 
 	@Test
-	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
-
-		RestAssured.given().accept(ContentType.JSON).when().get().then().body("", Matchers.hasSize(4)).body("nome",
-				Matchers.hasItems("Indiana", "Tailandesa"));
+	public void deveRetornarQuantidadeCorretaDeCozinhas_QuandoConsultarCozinhas() {
+		RestAssured.given().accept(ContentType.JSON).when().get().then().body("",
+				Matchers.hasSize(quantidadeCozinhasCadastradas));
 	}
 
-	// TESTE DE INTEGRAÇÃO
+	@Test
+	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+		RestAssured.given().body(jsonCorretoCozinhaChinesa).contentType(ContentType.JSON).accept(ContentType.JSON)
+				.when().post().then().statusCode(HttpStatus.CREATED.value());
+	}
+
+	@Test
+	public void deveRetornarRespostaEStatusCorretos_QuandoConsultarCozinhaExistente() {
+		RestAssured.given().pathParam("cozinhaId", cozinhaAmericana.getId()).accept(ContentType.JSON).when()
+				.get("/{cozinhaId}").then().statusCode(HttpStatus.OK.value())
+				.body("nome", Matchers.equalTo(cozinhaAmericana.getNome()));
+	}
+
+	@Test
+	public void deveRetornarStatus404_QuandoConsultarCozinhaInexistente() {
+		RestAssured.given().pathParam("cozinhaId", COZINHA_ID_INEXISTENTE).accept(ContentType.JSON).when()
+				.get("/{cozinhaId}").then().statusCode(HttpStatus.NOT_FOUND.value());
+	}
+
+	private void prepararDados() {
+		Cozinha cozinhaTailandesa = new Cozinha();
+		cozinhaTailandesa.setNome("Tailandesa");
+		cozinhaRepository.save(cozinhaTailandesa);
+
+		cozinhaAmericana = new Cozinha();
+		cozinhaAmericana.setNome("Americana");
+		cozinhaRepository.save(cozinhaAmericana);
+
+		quantidadeCozinhasCadastradas = (int) cozinhaRepository.count();
+	}
+
+}
+
+// TESTE DE INTEGRAÇÃO
 
 //	@Autowired
 //	CadastroCozinhaService cadastroCozinhaService;
@@ -92,5 +145,3 @@ class CadastroCozinhaIT {
 //		assertThat(erroEsperado).isNotNull();
 //
 //	}
-
-}
