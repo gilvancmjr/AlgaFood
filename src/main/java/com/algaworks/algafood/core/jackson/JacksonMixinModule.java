698864e683c65087edbdc717eec43b1599961526
Gilvan Júnior <juniormonteiroti@gmail.com>
2022-12-03 15:41:51 -0300

    Implementação CidadeMixin

--- src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -2,7 +2,9 @@ package com.algaworks.algafood.core.jackson;
 
 import org.springframework.stereotype.Component;
 
+import com.algaworks.algafood.api.model.mixin.CidadeMixin;
 import com.algaworks.algafood.api.model.mixin.RestauranteMixin;
+import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
@@ -16,6 +18,7 @@ public class JacksonMixinModule extends SimpleModule {
 
 	public JacksonMixinModule() {
 		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+		setMixInAnnotation(Cidade.class, CidadeMixin.class);
 	}
 
 }
