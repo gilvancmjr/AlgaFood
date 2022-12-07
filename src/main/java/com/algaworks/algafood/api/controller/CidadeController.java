2019fb423dafcb458e78ecf8a0b72d8e9028f161
Gilvan Júnior <juniormonteiroti@gmail.com>
2022-12-06 21:14:54 -0300

    Implementação Dto Cidade

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.api.assembler.CidadeInputDisassembler;
+import com.algaworks.algafood.api.assembler.CidadeModelAssembler;
+import com.algaworks.algafood.api.model.CidadeModel;
+import com.algaworks.algafood.api.model.input.CidadeInput;
 import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Cidade;
@@ -33,43 +35,53 @@ public class CidadeController {
 	@Autowired
 	private CadastroCidadeService cadastroCidade;
 	
+	@Autowired
+	private CidadeModelAssembler cidadeModelAssembler;
+
+	@Autowired
+	private CidadeInputDisassembler cidadeInputDisassembler;  
+	
 	@GetMapping
-	public List<Cidade> listar() {
-		return cidadeRepository.findAll();
+	public List<CidadeModel> listar() {
+	    List<Cidade> todasCidades = cidadeRepository.findAll();
+	    
+	    return cidadeModelAssembler.toCollectionModel(todasCidades);
 	}
-	
+
 	@GetMapping("/{cidadeId}")
-	public Cidade buscar(@PathVariable Long cidadeId) {
-		return cadastroCidade.buscarOuFalhar(cidadeId);
+	public CidadeModel buscar(@PathVariable Long cidadeId) {
+	    Cidade cidade = cadastroCidade.buscarOuFalhar(cidadeId);
+	    
+	    return cidadeModelAssembler.toModel(cidade);
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cidade adicionar(@RequestBody @Valid Cidade cidade) {
-		try {
-			return cadastroCidade.salvar(cidade);
-		} catch (EstadoNaoEncontradoException e) {
-			throw new NegocioException(e.getMessage(), e);
-		}
+	public CidadeModel adicionar(@RequestBody @Valid CidadeInput cidadeInput) {
+	    try {
+	        Cidade cidade = cidadeInputDisassembler.toDomainObject(cidadeInput);
+	        
+	        cidade = cadastroCidade.salvar(cidade);
+	        
+	        return cidadeModelAssembler.toModel(cidade);
+	    } catch (EstadoNaoEncontradoException e) {
+	        throw new NegocioException(e.getMessage(), e);
+	    }
 	}
-	
+
 	@PutMapping("/{cidadeId}")
-	public Cidade atualizar(@PathVariable Long cidadeId,
-			@RequestBody @Valid Cidade cidade) {
-		try {
-			Cidade cidadeAtual = cadastroCidade.buscarOuFalhar(cidadeId);
-			
-			BeanUtils.copyProperties(cidade, cidadeAtual, "id");
-			
-			return cadastroCidade.salvar(cidadeAtual);
-		} catch (EstadoNaoEncontradoException e) {
-			throw new NegocioException(e.getMessage(), e);
-		}
-	}
-	
-	@DeleteMapping("/{cidadeId}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void remover(@PathVariable Long cidadeId) {
-		cadastroCidade.excluir(cidadeId);	
+	public CidadeModel atualizar(@PathVariable Long cidadeId,
+	        @RequestBody @Valid CidadeInput cidadeInput) {
+	    try {
+	        Cidade cidadeAtual = cadastroCidade.buscarOuFalhar(cidadeId);
+	        
+	        cidadeInputDisassembler.copyToDomainObject(cidadeInput, cidadeAtual);
+	        
+	        cidadeAtual = cadastroCidade.salvar(cidadeAtual);
+	        
+	        return cidadeModelAssembler.toModel(cidadeAtual);
+	    } catch (EstadoNaoEncontradoException e) {
+	        throw new NegocioException(e.getMessage(), e);
+	    }
 	}
 }
