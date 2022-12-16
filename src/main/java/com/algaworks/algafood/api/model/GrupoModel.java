922db9bca6325568df55b4c88b00cdc13b642099
Gilvan Júnior <juniormonteiroti@gmail.com>
2022-12-15 21:15:15 -0300

    Implementação Grupo

---
+++ src/main/java/com/algaworks/algafood/api/model/GrupoModel.java
@@ -0,0 +1,14 @@
+package com.algaworks.algafood.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GrupoModel {
+
+	private Long id;
+
+	private String nome;
+
+}
