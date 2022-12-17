70a9a426ccb7a39e53f5ffae299c0b7ff2fa64e9
Gilvan Júnior <juniormonteiroti@gmail.com>
2022-12-17 16:14:35 -0300

    validação de email

--- src/main/java/com/algaworks/algafood/domain/repository/CustomJpaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CustomJpaRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
-public interface CustomJpaRepository<T, ID> extends JpaRepository<T, ID>{
-	
+public interface CustomJpaRepository<T, ID> extends JpaRepository<T, ID> {
+
 	Optional<T> buscarPrimeiro();
+
+	void detach(T entity);
 
 }
