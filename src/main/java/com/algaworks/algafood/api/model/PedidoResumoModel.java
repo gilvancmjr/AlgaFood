94b176f9a5955bd099f7a4a6b4de66c4e104b9cd
gilvancarvalho <gilvancarvalho@sefaz.pi.gov.br>
2022-12-21 11:21:02 -0300

    Atualização consulta pedidos

---
+++ src/main/java/com/algaworks/algafood/api/model/PedidoResumoModel.java
@@ -0,0 +1,26 @@
+package com.algaworks.algafood.api.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import com.algaworks.algafood.api.model.input.UsuarioModel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PedidoResumoModel {
+
+    private Long id;
+    private BigDecimal subtotal;
+    private BigDecimal taxaFrete;
+    private BigDecimal valorTotal;
+    private String status;
+    private OffsetDateTime dataCriacao;
+    private RestauranteResumoModel restaurante;
+    private UsuarioModel cliente;   
+}
+
+
--- src/main/java/com/algaworks/algafood/domain/repository/PedidoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/PedidoRepository.java
@@ -1,11 +1,17 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+
+    @Query("from Pedido p join fetch p.cliente join fetch p.restaurante r join fetch r.cozinha")
+    List<Pedido> findAll();
 
 }
