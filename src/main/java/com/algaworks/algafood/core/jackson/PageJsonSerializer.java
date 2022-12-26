89922a185281e50afa7646d04e79d9d8ac5925c7
Gilvan Júnior <juniormonteiroti@gmail.com>
2022-12-26 12:37:26 -0300

    criação do PageJsonSerializer

---
+++ src/main/java/com/algaworks/algafood/core/jackson/PageJsonSerializer.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.core.jackson;
+
+import java.io.IOException;
+
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+@JsonComponent
+public class PageJsonSerializer extends JsonSerializer<Page<?>> {
+
+    @Override
+    public void serialize(Page<?> page, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeStartObject();
+        gen.writeObjectField("content", page.getContent());
+        gen.writeNumberField("size", page.getSize());
+        gen.writeNumberField("TotalElements", page.getTotalElements());
+        gen.writeNumberField("totalPages", page.getTotalPages());
+        gen.writeNumberField("number", page.getNumber());
+        gen.writeNumberField("Status", HttpStatus.OK.value());
+        gen.writeEndObject();
+
+    }
+
+}
