2d2b10fbca57da93d88ab38c7040f97cf7fa0ae5
Gilvan Júnior <juniormonteiroti@gmail.com>
2022-12-31 14:19:14 -0300

    Implementação do pdf no controller

--- src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,11 +19,17 @@ public class EstatisticasController {
 
 	@Autowired
 	private VendaQueryService vendaQueryService;
-	
-	@GetMapping("/vendas-diarias")
+
+	@GetMapping(value = "/vendas-diarias", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro,
 			@RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
 		return vendaQueryService.consultarVendasDiarias(filtro, timeOffset);
 	}
-	
+
+	@GetMapping(value = "/vendas-diarias", produces = MediaType.APPLICATION_PDF_VALUE)
+	public List<VendaDiaria> consultarVendasDiariasPdf(VendaDiariaFilter filtro,
+			@RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
+		return vendaQueryService.consultarVendasDiarias(filtro, timeOffset);
+	}
+
 }
