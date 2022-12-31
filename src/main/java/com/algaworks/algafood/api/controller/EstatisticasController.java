6d6270f0a51cd5a60850d07a1eae866736c59065
Gilvan Júnior <juniormonteiroti@gmail.com>
2022-12-31 18:24:10 -0300

    implementação do PDF

--- src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
@@ -3,7 +3,9 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.Relatorio.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
+import com.algaworks.algafood.domain.service.VendaReportService;
 
 @RestController
 @RequestMapping(path = "/estatisticas")
@@ -20,16 +23,28 @@ public class EstatisticasController {
 	@Autowired
 	private VendaQueryService vendaQueryService;
 
-	@GetMapping(value = "/vendas-diarias", produces = MediaType.APPLICATION_JSON_VALUE)
+	@Autowired
+	private VendaReportService vendaReportService;
+
+	@GetMapping("/vendas-diarias")
 	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro,
 			@RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
 		return vendaQueryService.consultarVendasDiarias(filtro, timeOffset);
 	}
 
-	@GetMapping(value = "/vendas-diarias", produces = MediaType.APPLICATION_PDF_VALUE)
-	public List<VendaDiaria> consultarVendasDiariasPdf(VendaDiariaFilter filtro,
+	@GetMapping("/vendas-diarias-pdf")
+	public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro,
 			@RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
-		return vendaQueryService.consultarVendasDiarias(filtro, timeOffset);
+
+		byte[] bytesPdf = vendaReportService.emitirVendasDiarias(filtro, timeOffset);
+		var headers = new HttpHeaders();
+		headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=vendas-diarias.pdf");
+
+		return ResponseEntity.ok()
+				.contentType(MediaType.APPLICATION_PDF)
+				.headers(headers)
+				.body(bytesPdf);
+
 	}
 
 }
