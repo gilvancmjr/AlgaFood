6a8830eecd7941afaaddac3c773ab1ea644ffb94
gilvancarvalho <gilvancarvalho@sefaz.pi.gov.br>
2023-03-31 08:28:27 -0300

    implementação swagger

--- src/main/java/com/algaworks/algafood/AlgafoodApplication.java
+++ src/main/java/com/algaworks/algafood/AlgafoodApplication.java
@@ -5,9 +5,11 @@ import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.algaworks.algafood.infrastructure.repository.CustomJpaRepositoryImpl;
 
+@EnableWebMvc
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class AlgafoodApplication {
