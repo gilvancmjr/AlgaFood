68c30f7fd9ee2e9a6e714edd5d615c1f97c339cd
gilvancarvalho <gilvancarvalho@sefaz.pi.gov.br>
2023-04-05 13:02:24 -0300

    update

--- src/main/java/com/algaworks/algafood/core/openapi/model/PageableModelOpenApi.java
+++ src/main/java/com/algaworks/algafood/core/openapi/model/PageableModelOpenApi.java
@@ -11,14 +11,11 @@ import lombok.Setter;
 @Setter
 @Getter
 public class PageableModelOpenApi {
-	
 
 	@ApiModelProperty(example = "0", value = "Número da página (começa em 0)")
 	private int page;
-	
 	@ApiModelProperty(example = "10", value = "Quantidade de elementos por página")
 	private int size;
-	
 	@ApiModelProperty(example = "nome,asc", value = "Nome da propriedade para ordenação")
 	private List<String> sort;
 }
