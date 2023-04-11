765f33af1e5f6bbea1bf858b6ca14d76ae23ca3c
gilvancarvalho <gilvancarvalho@sefaz.pi.gov.br>
2023-04-11 07:54:49 -0300

    update

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutosController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutosController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.algaworks.algafood.api.assembler.ProdutoInputDisassembler;
 import com.algaworks.algafood.api.assembler.ProdutoModelAssembler;
 import com.algaworks.algafood.api.model.ProdutoModel;
 import com.algaworks.algafood.api.model.input.ProdutoInput;
+import com.algaworks.algafood.api.openapi.controller.RestauranteProdutoControllerOpenApi;
 import com.algaworks.algafood.domain.model.Produto;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.ProdutoRepository;
@@ -28,7 +30,7 @@ import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos")
-public class RestauranteProdutosController {
+public class RestauranteProdutosController implements RestauranteProdutoControllerOpenApi {
 
 	@Autowired
 	private CadastroProdutoService cadastroProdutoService;
@@ -45,7 +47,7 @@ public class RestauranteProdutosController {
 	@Autowired
 	private ProdutoInputDisassembler produtoInputDisassembler;
 
-	@GetMapping
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<ProdutoModel> listar(@PathVariable Long restauranteId,
 			@RequestParam(required = false) boolean incluirInativos) {
 		Restaurante restaurante = cadastroRestauranteService.buscarOuFalhar(restauranteId);
@@ -60,14 +62,14 @@ public class RestauranteProdutosController {
 		return produtoModelAssembler.toCollectionModel(todosProdutos);
 	}
 
-	@GetMapping("/{produtoId}")
+	@GetMapping(value = "/{produtoId}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ProdutoModel buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
 		Produto produto = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
 
 		return produtoModelAssembler.toModel(produto);
 	}
 
-	@PostMapping
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.CREATED)
 	public ProdutoModel adicionar(@PathVariable Long restauranteId, @RequestBody @Valid ProdutoInput produtoInput) {
 		Restaurante restaurante = cadastroRestauranteService.buscarOuFalhar(restauranteId);
@@ -80,7 +82,7 @@ public class RestauranteProdutosController {
 		return produtoModelAssembler.toModel(produto);
 	}
 
-	@PutMapping("/{produtoId}")
+	@PutMapping(value = "/{produtoId}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ProdutoModel atualizar(@PathVariable Long restauranteId, @PathVariable Long produtoId,
 			@RequestBody @Valid ProdutoInput produtoInput) {
 		Produto produtoAtual = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
--- src/main/java/com/algaworks/algafood/api/model/ProdutoModel.java
+++ src/main/java/com/algaworks/algafood/api/model/ProdutoModel.java
@@ -2,21 +2,22 @@ package com.algaworks.algafood.api.model;
 
 import java.math.BigDecimal;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ProdutoModel {
-
+	@ApiModelProperty(example = "1")
 	private Long id;
-
+	@ApiModelProperty(example = "Espetinho de Cupim")
 	private String nome;
-
+	@ApiModelProperty(example = "Acompanha farinha, mandioca e vinagrete")
 	private String descricao;
-
+	@ApiModelProperty(example = "12.50")
 	private BigDecimal preco;
-
+	@ApiModelProperty(example = "true")
 	private Boolean ativo;
 
 }
