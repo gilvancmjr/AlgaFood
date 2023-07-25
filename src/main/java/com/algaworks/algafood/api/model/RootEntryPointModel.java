d8907b88cc340b8a7921a27c6a973223c654fa38
Gilvan Júnior <98542272+gilvancmjr@users.noreply.github.com>
2023-07-25 13:42:41 -0300

    implementação do RootEntryPointModel

---
+++ src/main/java/com/algaworks/algafood/api/model/RootEntryPointModel.java
@@ -0,0 +1,7 @@
+package com.algaworks.algafood.api.model;
+
+import org.springframework.hateoas.RepresentationModel;
+
+public class RootEntryPointModel extends RepresentationModel<RootEntryPointModel>{
+
+}
